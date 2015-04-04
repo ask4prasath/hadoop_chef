05c8e3aba1a09d85447b2e1a8e9911f1a46fe225
Prasath <ask4prasath@gmail.com>
2015-04-04 19:10:50 +0000

    Added start script for zookepper

--- cookbooks/rhea_chef_repo/recipes/default.rb
+++ cookbooks/rhea_chef_repo/recipes/default.rb
@@ -5,3 +5,9 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe "zookeeper"
+
+service 'zookeeper' do  
+  provider Chef::Provider::Service::Upstart
+  start_command "/opt/zookeeper/zookeeper-3.4.6/bin/zkServer.sh start"
+  action [:start]
+end

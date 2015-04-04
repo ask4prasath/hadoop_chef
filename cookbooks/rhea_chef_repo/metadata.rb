8c9f9452194d418b4ea55c2aaf7b5aa787429e93
prasath <ask4prasath@gmail.com>
2015-04-04 18:18:31 +0530

    Added cookbooks

---
+++ cookbooks/rhea_chef_repo/metadata.rb
@@ -0,0 +1,12 @@
+name             'rhea_chef_repo'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures rhea_chef_repo'
+long_description 'Installs/Configures rhea_chef_repo'
+version          '0.1.0'
+
+depends "build-essential"
+depends "zookeeper"
+
+suggests "homebrew"
---
+++ cookbooks/rhea_chef_repo/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: rhea_chef_repo
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+include_recipe "zookeeper"
